9345236ddd3ce1af4ffc533f0ee1d5e37a593f3f
Amit-CsEDev <amits.csebwndevlpr@gmail.com>
2020-04-29 01:50:01 +0530

    Add Base Code 1
    Adding Base Codes and Files

---
+++ UIModule.js
@@ -0,0 +1,12 @@
+var UIModule = (function(){
+
+    // classes used to select HTML elements 
+
+    var DOMElements = {
+        // indicators - test control
+        timeLeft,//HTML element displaying time Left
+        //test results 
+        wpm,wpm
+    }
+
+})
