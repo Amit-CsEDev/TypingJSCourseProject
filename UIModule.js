c12b13c06bbed79b6578e804ecd7896a8a35e486
Amit-CsEDev <amits.csebwndevlpr@gmail.com>
2020-04-29 09:32:14 +0530

    Add Base Codes 2
    Add New Base Files and Codes Part 2

--- UIModule.js
+++ UIModule.js
@@ -6,7 +6,36 @@ var UIModule = (function(){
         // indicators - test control
         timeLeft,//HTML element displaying time Left
         //test results 
-        wpm,wpm
-    }
+        wpm,wpmChange,cpm,cpmChange,accuracy,accuracyChange, // user input
+        textInput,nameInput, // test words 
+        content,activeWord, // modal 
+        modal
+    };
 
-})
+    return {
+        // get DOM elements 
+        getDOMElements(){},
+        // Indicators - Test Control 
+        updateTimeLeft : function(){},
+        // results
+        updateResults : function(){},
+        fillModal : function(){},
+        showModal : function(){},
+        // user input
+        inputfocus : function(){},
+        isNameEmpty : function(){},
+        flagNameInput : function(){},
+        spacePressed : function(){},
+        enterPressed : function(){},
+        emptyInput : function(){},
+        getTypedWord : function(){},
+
+        // test words
+
+        fillContent : function(){},
+        formatWord : function(wordObject, wordHTML){},
+        setActiveword : function(index){},
+        deactivateCurrentWord : function(){},
+        scroll : function(){}
+    }
+})();
---
+++ eventsModule.js
@@ -0,0 +1,14 @@
+var eventsModule = (function(){
+    var addEventListeners = function(){
+        // character typing error listener 
+        // click on download button event listener 
+        // click on restart button event listener 
+    };
+    return {
+        // init function, initializes the test before start 
+        init : function(duration,textNumber){
+            addEventListeners();
+        }
+    };
+
+})
