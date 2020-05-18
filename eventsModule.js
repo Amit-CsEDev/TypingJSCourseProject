7c01608b67b219f5514a336f086df5d7eea7ba82
Amit-CsEDev <amits.csebwndevlper@gmail.com>
2020-05-18 23:48:21 +0530

    Minor Changes
    Minor Changes

--- eventsModule.js
+++ eventsModule.js
@@ -1,7 +1,11 @@
 var eventsModule = (function(dModule,cModule,uModule,wModule){
     var addEventListeners = function(){
         // character typing error listener 
+
         // click on download button event listener 
+        uModule.getDOMElements().textInput.addEventListeners('',function(event){
+            
+        });
         // click on restart button event listener  done through location.reload function in html page in restart button
     };
     return {
